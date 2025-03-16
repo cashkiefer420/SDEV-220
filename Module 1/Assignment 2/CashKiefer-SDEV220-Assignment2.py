d6c745afc799bf56305e014ad724df8e52cbd7f4
Cash Kiefer <cash.kiefer2008@gmail.com>
2025-03-16 09:26:38 -0500

    Assignment 2.py updated

---
+++ Module 1/Assignment 2/CashKiefer-SDEV220-Assignment2.py
@@ -0,0 +1,15 @@
+print(60*60)
+
+seconds_per_hour = 60*60
+print(seconds_per_hour)
+
+print(seconds_per_hour*24)
+
+seconds_per_day = seconds_per_hour*24
+print(seconds_per_day)
+
+print(seconds_per_day/seconds_per_hour)
+
+print(seconds_per_day//seconds_per_hour)
+
+#The print(seconds_per_day//seconds_per_hour) had no decimal. 
