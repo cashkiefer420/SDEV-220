d7e79e585d3235d590355f8361eace7c80466f90
Cash Kiefer <cash.kiefer2008@gmail.com>
2025-03-24 09:10:05 -0500

    M03A1 Added

---
+++ Module 3/CashKiefer-SDEV220-M03A1.py
@@ -0,0 +1,18 @@
+# Creating a list with the specified elements
+things = ["mozzarella", "cinderella", "salmonella"]
+
+# Printing the list to verify
+print(things)
+
+# Capitalizing the element referring to a person
+things[1] = things[1].capitalize()
+
+# Printing the list to check if it changed
+print(things)
+#ELEMENT IS CHANGED
+
+#Completely capitalizing mozzerella
+things[0] = things[0].upper()
+
+# Printing the list to check if it changed
+print(things)
