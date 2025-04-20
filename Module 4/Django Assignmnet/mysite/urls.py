3b691d3ca9ca6ae8e68a3edde3b0abfdadb13a12
Cash Kiefer <cash.kiefer2008@gmail.com>
2025-04-20 15:48:41 -0500

    Django Pt2

--- Module 4/Django Assignmnet/mysite/urls.py
+++ Module 4/Django Assignmnet/mysite/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path
+"""mysite URL Configuration
+
+[...]
+"""
+from django.contrib import admin # type: ignore
+from django.urls import path, include # type: ignore
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
