faf9690df9c2c2f9c87fc472be043bd3dbf13bc4
Cash Kiefer <cash.kiefer2008@gmail.com>
2025-04-27 07:29:45 -0500

    Django Tutorial Finish

--- Module 4/Django Assignmnet/blog/views.py
+++ Module 4/Django Assignmnet/blog/views.py
@@ -1,6 +1,10 @@
+from django.utils import timezone  # Correct import for timezone
 from django.shortcuts import render  # type: ignore
+from .models import Post
+
 
 # Create your views here.
 
 def post_list(request):
+    Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {})
