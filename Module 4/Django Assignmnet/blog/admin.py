4fd992032d57cae8bf3141bd02344ce082cdcbf9
Cash Kiefer <cash.kiefer2008@gmail.com>
2025-04-30 08:14:28 -0500

    finish django

--- Module 4/Django Assignmnet/blog/admin.py
+++ Module 4/Django Assignmnet/blog/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_date')
+    actions = ['publish_posts']
+
+    def publish_posts(self, request, queryset):
+        for post in queryset:
+            post.publish()
+    publish_posts.short_description = "Publish selected posts"
+
+admin.site.register(Post, PostAdmin)
--- Module 4/Django Assignmnet/blog/views.py
+++ Module 4/Django Assignmnet/blog/views.py
@@ -1,10 +1,43 @@
-from django.utils import timezone  # Correct import for timezone
-from django.shortcuts import render  # type: ignore
+from django.shortcuts import render, redirect
+from django.utils import timezone
 from .models import Post
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm  # You’ll need to create this form
 
 
-# Create your views here.
+
 
 def post_list(request):
-    Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            post.publish()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def create_post(request):
+    post = Post(title="New Post", text="Text here")
+    post.save()
+    post.publish()
+
+from django.shortcuts import get_object_or_404
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+from django.contrib.auth import logout
+
+def logout_view(request):
+    logout(request)
+    return redirect('post_list')  # or wherever you want users to go after logging out
